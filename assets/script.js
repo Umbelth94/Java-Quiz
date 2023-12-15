9a8f4cb1cf6372bfc10e6096aaccfd6975a51876
Travis Umbel <Umbelth94@gmail.com>
2023-12-15 13:43:01 -0600

    Add JS file, Jquery CDN, and basic questions to start off

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+//Array of question objects
+var questions = [
+    {
+        title:'What type of language is Javascript?',
+        choices:['Object-Oriented', 'Object-Based','Procedural','None of the Above'],
+        answer: 'Object-Oriented'
+    },
+    {
+        title:'Which of the following keywords is used to define a variable in Javascript?',
+        choices:['var','let','Both A and B', 'None of the Above'],
+        answer:'Both A and B'
+    },
+    {
+        title:'Which of the following methods is used to access HTML elements using Javascript?',
+        choices:['getElementbyId()','getElementsByClassName()','Both A and B','None of the above'],
+        answer:'Both A and B'
+    }
+]
+
