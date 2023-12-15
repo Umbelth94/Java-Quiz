c7d30bc36daaf44722aea74d8f809e037a2d9277
Travis Umbel <Umbelth94@gmail.com>
2023-12-15 14:53:31 -0600

    Added questions array shuffler

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,19 @@ var questions = [
         answer:'Both A and B'
     }
 ]
+//Shuffle the questions array so that the questions appear in a different order every run.
+function shuffle(array){
+    let currentIndex = array.length, randomIndex;
+
+    while(currentIndex > 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
 
 var timerCount = 90;
 var timer = $('#timer');
@@ -28,8 +41,12 @@ var questionContainer = $('#question-container');
 
 function startQuiz(){
     startContainer.hide();
+    shuffle(questions);
     var timerInterval = setInterval(countDown, 1000);
-    
+    //Create Elements from the questions array and append them to the questioncontainer.
+
+
+
     function countDown(){
         timer.text('Timer: ' + timerCount);
         if (timerCount === 0){
@@ -39,6 +56,10 @@ function startQuiz(){
         timerCount --;
         }
     }
+}
+
+function displayQuestion(randomlySortedQuestions){
+    var question = 0;
 }
 
 
